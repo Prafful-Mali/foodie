7eb7e813fd5eb2aee99e3caf7764c4b9e63e8d2b
Prafful Mali <prafful@gkmit.co>
2026-01-04 00:19:58 +0530

    feat: implement Ingredient API with ViewSet, permissions and pagination

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CuisineViewSet
+from .views import CuisineViewSet, IngredientViewSet
 
 router = DefaultRouter()
 router.register(r"cuisines", CuisineViewSet, basename="cuisine")
+router.register(r"ingredients", IngredientViewSet, basename="ingredient")
+
 
 urlpatterns = [
     path("", include(router.urls)),
--- recipes/views.py
+++ recipes/views.py
@@ -3,9 +3,9 @@ from django.utils import timezone
 from rest_framework.response import Response
 from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
-from .models import Cuisine
+from .models import Cuisine, Ingredient
 from .permissions import IsAdmin
-from .serializers import CuisineSerializer
+from .serializers import CuisineSerializer, IngredientSerializer
 from .pagination import DefaultPagination
 
 
@@ -49,4 +49,58 @@ class CuisineViewSet(viewsets.ViewSet):
         cuisine = get_object_or_404(Cuisine, pk=pk, deleted_at__isnull=True)
         cuisine.deleted_at = timezone.now()
         cuisine.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class IngredientViewSet(viewsets.ViewSet):
+
+    def get_permissions(self):
+        if self.action in ["create", "partial_update", "destroy"]:
+            permission_classes = [IsAuthenticated, IsAdmin]
+        else:
+            permission_classes = [IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
+
+    def list(self, request):
+        ingredients = Ingredient.objects.filter(deleted_at__isnull=True)
+
+        paginator = DefaultPagination()
+        paginated_qs = paginator.paginate_queryset(ingredients, request)
+
+        serializer = IngredientSerializer(paginated_qs, many=True)
+        return paginator.get_paginated_response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        ingredient = get_object_or_404(Ingredient, pk=pk, deleted_at__isnull=True)
+        serializer = IngredientSerializer(ingredient)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        name = request.data.get("name")
+
+        old = Ingredient.objects.filter(name=name, deleted_at__isnull=False).first()
+
+        if old:
+            old.deleted_at = None
+            old.save()
+            serializer = IngredientSerializer(old)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        serializer = IngredientSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def partial_update(self, request, pk=None):
+        ingredient = get_object_or_404(Ingredient, pk=pk, deleted_at__isnull=True)
+        serializer = IngredientSerializer(ingredient, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def destroy(self, request, pk=None):
+        ingredient = get_object_or_404(Ingredient, pk=pk, deleted_at__isnull=True)
+        ingredient.deleted_at = timezone.now()
+        ingredient.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
