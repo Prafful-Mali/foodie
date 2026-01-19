563d738bd32eea3421559d88ee3e534837f40ac4
Prafful Mali <prafful@gkmit.co>
2026-01-19 16:34:05 +0530

    chore: remove unintended changes

--- common/middleware.py
+++ common/middleware.py
@@ -1,35 +1,3 @@
-# from rest_framework_simplejwt.authentication import JWTAuthentication
-# from rest_framework.exceptions import AuthenticationFailed
-
-
-# class TenantMiddleware:
-#     """
-#     Middleware to automatically attach tenant to request object.
-#     Handles both session-based and JWT authentication.
-#     """
-#     def __init__(self, get_response):
-#         self.get_response = get_response
-#         self.jwt_authenticator = JWTAuthentication()
-
-#     def __call__(self, request):
-#         # Try to authenticate via JWT if Authorization header is present
-#         if request.path.startswith('/api/') and 'Authorization' in request.headers:
-#             try:
-#                 auth_result = self.jwt_authenticator.authenticate(request)
-#                 if auth_result is not None:
-#                     request.user, _ = auth_result
-#             except (AuthenticationFailed, Exception):
-#                 pass  # Let DRF handle authentication errors
-
-#         # Add tenant to request if user is authenticated
-#         if hasattr(request, 'user') and request.user.is_authenticated:
-#             request.tenant = getattr(request.user, 'tenant', None)
-#         else:
-#             request.tenant = None
-
-#         response = self.get_response(request)
-#         return response
-
 from django.contrib.auth.models import AnonymousUser
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.exceptions import AuthenticationFailed
