224e02763d88791be56c386c49e2b9ed2a7e03a5
Prafful Mali <prafful@gkmit.co>
2026-01-21 23:37:59 +0530

    feat: add subscription, payment, and webhook event models

---
+++ payments/enums.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class SubscriptionStatus(models.TextChoices):
+    PENDING = "pending", "Pending"
+    PAID = "paid", "Paid"
+    FAILED = "failed", "Failed"
+
+
+class PaymentStatus(models.TextChoices):
+    CREATED = "created", "Created"
+    CAPTURED = "captured", "Captured"
+    FAILED = "failed", "Failed"
