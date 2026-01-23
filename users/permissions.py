3f6a96321a8e053c4778af8a3878ed11ce8e0250
Prafful Mali <prafful@gkmit.co>
2026-01-23 23:57:47 +0530

    refactor: used enums from common/enums everywhere

--- users/permissions.py
+++ users/permissions.py
@@ -1,18 +1,19 @@
 from rest_framework.permissions import BasePermission
+from common.enums import UserRole
 
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
         return (
             request.user.is_authenticated
-            and (request.user.role == "ADMIN" or request.user.is_superadmin)
+            and (request.user.role == UserRole.ADMIN or request.user.is_superadmin)
             and request.user.is_active
         )
 
 
 class IsOwnerOrAdmin(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.user.role == "ADMIN":
+        if request.user.role == UserRole.ADMIN:
             return True
         return obj == request.user
 
@@ -20,12 +21,12 @@ class IsOwnerOrAdmin(BasePermission):
 class CanDeleteUser(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.user.is_superadmin:
-            return obj.role == "ADMIN"
+            return obj.role == UserRole.ADMIN
 
-        if request.user.role == "ADMIN":
+        if request.user.role == UserRole.ADMIN:
             return (
                 obj != request.user
                 and obj.tenant_id == request.tenant.id
-                and obj.role == "USER"
+                and obj.role == UserRole.USER
             )
         return obj == request.user
